95582b7e733f5021d9623a8c4d794f1f9447fbd7
Marcin Swornowski <swor71@gmail.com>
2018-03-31 15:40:25 +0200

    Add basic styling to Contact component

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Contact = (props) => (
 	<li className='list-group-item'>
-		<img className='img-circle media-object pull-left' src={props.picture} width='32' height='32' />
-		<div className='media-body'>
-			<strong>Name: {props.name}</strong>
-			<strong>Name: {props.surname}</strong>
-			<span>Phone number: {props.phone}</span>
+		<img className='img-circle media-object pull-left' src={props.picture} alt='avatar' width='32' height='32' />
+		<div className='media-body contact-info'>
+			<strong>Name: <span className='not-bold'>{props.firstName}</span></strong>
+			<strong>Surname: <span className='not-bold'>{props.surname}</span></strong>
+			<strong>Phone number: <span className='not-bold'>{props.phone}</span></strong>
 		</div>
 	</li>
 );
--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -6,7 +6,7 @@ const ContactList = (props) => (
 		<li className='list-group-header'>
 			<input className='form-control' type='text' placeholder='Search..' />
 		</li>
-		{props.data.map(contact => <Contact key={contact._id} name={contact.name} surname={contact.surname} phone={contact.phone}/> )}
+		{props.data.map(contact => <Contact key={contact._id} firstName={contact.firstName} surname={contact.surname} phone={contact.phone}/> )}
 	</ul>
 );
 
