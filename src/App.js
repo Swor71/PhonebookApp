fe35ff8a11c3de6a779fda6e61ef9fa0763a3f59
Michał Gacek <gacek.michael@outlook.com>
2018-03-31 13:30:41 +0200

    Electron boilerplate

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className={styles.App}>
+        <div className={styles.AppHeader}>
+          <img src={logo} className={styles.AppLogo} alt="logo" />
           <h2>Welcome to React/Electron</h2>
         </div>
-        <p className="App-intro">
+        <p className={styles.AppIntro}>
           Hello Electron!
         </p>
       </div>
